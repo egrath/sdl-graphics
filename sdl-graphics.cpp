6661cbb04ad0a4101d1fd9a693bbed5c08f7d1c8
Egon Rath <egon.rath@gmail.com>
2022-06-02 21:28:23 +0200

    added support for building on Mac OS

--- sdl-graphics.cpp
+++ sdl-graphics.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
+#ifdef __APPLE__
+    #include <SDL2/SDL.h>
+    #include <SDL2_image/SDL_image.h>
+    #include <SDL2_ttf/SDL_ttf.h>
+#else
+    #include <SDL2/SDL.h>
+    #include <SDL2/SDL_image.h>
+    #include <SDL2/SDL_ttf.h>
+#endif
 
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_sdl.h"
